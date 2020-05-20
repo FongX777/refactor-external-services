d2d2b4acc15d0289fe27df22619cd6e6f8fd8602
fong <fong@meepshop.com>
2020-05-21 02:04:55 +0800

    Successfully reuest and process data

---
+++ src/main/java/fong/refactor/ApiExample.java
@@ -0,0 +1,95 @@
+package fong.refactor;
+
+/**
+ * Sample Java code for youtube.videos.list
+ * See instructions for running these code samples locally:
+ * https://developers.google.com/explorer-help/guides/code_samples#java
+ */
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.YouTubeRequestInitializer;
+import com.google.api.services.youtube.model.VideoListResponse;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class ApiExample {
+    private static final String CLIENT_SECRETS= "client_secret.json";
+    private static final Collection<String> SCOPES =
+            Arrays.asList("https://www.googleapis.com/auth/youtube.readonly");
+
+    private static final String APPLICATION_NAME = "API code samples";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String API_KEY = "";
+
+
+    /**
+     * Build and return an authorized API client service.
+     *
+     * @return an authorized API client service
+     * @throws GeneralSecurityException, IOException
+     */
+    public static YouTube getService() throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+
+        return new YouTube.Builder(httpTransport, JSON_FACTORY, null)
+                .setApplicationName("API Project")
+                .setGoogleClientRequestInitializer(new YouTubeRequestInitializer(API_KEY))
+                .build();
+    }
+
+    /**
+     * Call function to create API service object. Define and
+     * execute API request. Print API response.
+     *
+     * @throws GeneralSecurityException, IOException, GoogleJsonResponseException
+     */
+    public static void main(String[] args)
+            throws GeneralSecurityException, IOException, GoogleJsonResponseException {
+        YouTube youtubeService = getService();
+        // Define and execute the API request
+        YouTube.Videos.List request = youtubeService.videos()
+                .list("snippet,contentDetails,statistics");
+        VideoListResponse response = request.setId("Ks-_Mh1QhMc,c0KYU2j0TM4,eIho2S0ZahI").execute();
+        System.out.println(response);
+    }
+
+    /**
+     * @param ids
+     * @param parts
+     * @return
+     * @example
+     * ids: ["Ks-_Mh1QhMc,c0KYU2j0TM4,eIho2S0ZahI"]
+     */
+    static public VideoListResponse requestVideoList(String[] ids, String parts[]) {
+        YouTube youtubeService = null;
+        try {
+            youtubeService = getService();
+            // Define and execute the API request
+            YouTube.Videos.List request = youtubeService.videos()
+                    .list(String.join(",", parts));
+            VideoListResponse response = request.setId(String.join(",", ids)).execute();
+            return response;
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
--- src/main/java/fong/refactor/VideoService.java
+++ src/main/java/fong/refactor/VideoService.java
@@ -1,44 +1,73 @@
 package fong.refactor;
 
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.util.DateTime;
+import com.google.api.services.youtube.model.Video;
+import com.google.api.services.youtube.model.VideoListResponse;
+import com.google.auth.oauth2.AccessToken;
+import com.google.auth.oauth2.GoogleCredentials;
 import org.json.JSONArray;
+import org.json.JSONObject;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.util.TimeZone;
 
 class VideoService {
     public static void main(String args[]) {
         System.out.println("Hello World");
         VideoService videoService = new VideoService();
         videoService.videoList();
-
-
-
     }
 
     public void videoList() {
         String filePath = "src/main/resources/videos.json";
 
         // https://stleary.github.io/JSON-java/
-        JSONArray array = new JSONArray(VideoService.readFile(filePath));
+        JSONArray videoList = new JSONArray(VideoService.readFile(filePath));
 
-        String[] ids = new String[array.length()];
-        for (int i=0; i< array.length(); i++) {
-            ids[i] = array.getJSONObject(i).getString("youtubeID");
+        String[] ids = new String[videoList.length()];
+        for (int i = 0; i < videoList.length(); i++) {
+            ids[i] = videoList.getJSONObject(i).getString("youtubeID");
         }
 
-//        GoogleCredential credential = new GoogleCredential().setAccessToken(accessToken);
-//        Plus plus = new Plus.builder(new NetHttpTransport(),
-//                JacksonFactory.getDefaultInstance(),
-//                credential)
-//                .setApplicationName("Google-PlusSample/1.0")
-//                .build();
+        VideoListResponse listResponse =
+                ApiExample.requestVideoList(ids, new String[]{"snippet", "contentDetails", "statistics"});
+
+
+        for (int i = 0; i < ids.length; i++) {
+            String id = ids[i];
+            JSONObject video = videoList.getJSONObject(i);
+            // https://dzone.com/articles/how-to-use-map-filter-collect-of-stream-in-java-8
+            Video youtubeRecord = listResponse.getItems().get(i);
+
+            int views = youtubeRecord.getStatistics().getViewCount().intValue();
+            video.put("views", views);
+
+            LocalDate publishedAt = LocalDate.ofEpochDay(youtubeRecord.getSnippet().getPublishedAt().getValue() / 1000 / 60 / 60 / 24);
+            LocalDate now = LocalDate.now();
+
+            // https://mkyong.com/java8/java-8-difference-between-two-localdate-or-localdatetime/
+            long daysAvailable = Duration.between(publishedAt.atStartOfDay(), now.atStartOfDay()).toDays();
+
+
+            System.out.println("---" + publishedAt + "," + now + "," + views + ", " + daysAvailable + ", ");
+            video.put("monthlyViews", views * 365 / daysAvailable / 12);
+        }
+
+        System.out.println(videoList.toString());
+        writeFile("src/main/resources/videos-output.json", videoList.toString());
     }
 
 
-
-    public static String readFile(String filePath)  {
+    public static String readFile(String filePath) {
         String content = null;
         try {
             content = Files.readString(Paths.get(filePath));
@@ -48,4 +77,11 @@ class VideoService {
         return content;
     }
 
+    public static void writeFile(String filePath, String fileString) {
+        try {
+            Files.write(Paths.get(filePath), fileString.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
